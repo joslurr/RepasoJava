a5eb1c5c350672fef83c16ff0ee5d8a7609c2ef4
joslurr <113449783+joslurr@users.noreply.github.com>
2024-09-20 22:29:36 +0200

    Añadido Ejercicio 101 - Suma de números.

--- src/repaso/Ejercicio101.java
+++ src/repaso/Ejercicio101.java
@@ -1,4 +1,19 @@
 package repaso;
 
 public class Ejercicio101 {
+
+    static String texto = "4;5;7;8;4";
+    public static void main(String[] args) {
+
+        int suma=0;
+
+        String[] separados = texto.split(";");
+
+        for (String separado : separados){
+            suma+=Integer.parseInt(separado);
+        }
+
+        System.out.println("El total es: " + suma);
+
+    }
 }
